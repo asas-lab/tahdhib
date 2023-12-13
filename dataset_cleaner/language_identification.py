97145bae914ba03bd7e6f2546f854ffbf3cf62c2
Khalidalt <ekh@protonmail.com>
2023-12-13 14:31:48 +0300

    update lang dete

--- dataset_cleaner/language_identification.py
+++ dataset_cleaner/language_identification.py
@@ -3,10 +3,24 @@ import fasttext
 from huggingface_hub import hf_hub_download
 
 
+import fasttext
+import numpy as np
+
+from huggingface_hub import hf_hub_download
+
+
 def fast_text(text):
     model_path = hf_hub_download(repo_id="facebook/fasttext-language-identification", filename="model.bin")
     model = fasttext.load_model(model_path)
-    result = model.predict(text,k=219)
+    preds, scores = model.predict(text,k=219)
+
+    if isinstance(scores, np.ndarray):
+        scores = scores.tolist()
+
+    for label, score in zip(preds, scores):
+        if label == '__label__arb_Arab':
+            return {'label': label, 'score': score}
+
     return {'label': result[0][0],
           'score': result[1][0]}
 
