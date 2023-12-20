832404cd558b5e67672ab76fbc1c4ce5f140dad7
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2023-12-19 22:40:35 -0500

    Update normalize_arabic.py
    added deleting empty lines from the text

--- normalize_arabic.py
+++ normalize_arabic.py
@@ -4,39 +4,50 @@ def normalize_arabic(text):
     # Arabic unicode block range
     arabic_range = '\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF'
 
-    # 1. Normalize Arabic diacritics (Tashkeel)
-    tashkeel_pattern = re.compile(r'[\u0617-\u061A\u064B-\u0652\u06D6-\u06DC\u06DF-\u06E8\u06EA-\u06ED]')
-    text = re.sub(tashkeel_pattern, '', text)
 
-    # 2. Standardize Arabic characters
-    # Replace Alif variants with bare Alif
-    text = re.sub(r'[\u0622\u0623\u0625\u0671\u0672\u0673\u0675]', '\u0627', text)
-    # Replace Yaa variants with bare Yaa
-    text = re.sub(r'[\u0649]', '\u064A', text)
+    # Function to apply normalization rules to a single text block or line
+    def normalize_text_block(text):
+        # 1. Normalize Arabic diacritics (Tashkeel)
+        tashkeel_pattern = re.compile(r'[\u0617-\u061A\u064B-\u0652\u06D6-\u06DC\u06DF-\u06E8\u06EA-\u06ED]')
+        text = re.sub(tashkeel_pattern, '', text)
 
-    # 3. Remove Tatweel (Kashida)
-    text = re.sub(r'\u0640', '', text)
-    
-    # 4. Remove non-Arabic characters
-    # text = re.sub(r'[^\s' + arabic_range + ']', '', text)
-    
-    # 5. Normalize whitespace
-    text = ' '.join(text.split())
-    
+        # 2. Standardize Arabic characters
+        # Replace Alif variants with bare Alif
+        text = re.sub(r'[\u0622\u0623\u0625\u0671\u0672\u0673\u0675]', '\u0627', text)
+        # Replace Yaa variants with bare Yaa
+        text = re.sub(r'[\u0649]', '\u064A', text)
+
+        # 3. Remove Tatweel (Kashida)
+        text = re.sub(r'\u0640', '', text)
+                
+        # 4.Normalize whitespace
+        text = ' '.join(text.split())
+
+        return text
+
+    # 5. Remove empty lines 
+    # if it is a text block
+    if isinstance(text, str):
+        text = normalize_text_block(text)
+        text = re.sub(r'\n+', '\n', text)  # Replace multiple newlines with a single one
+        text = re.sub(r'^\s*$', '', text, flags=re.MULTILINE)  # Remove empty lines
+
+    # if it is line in a list
+    elif isinstance(text, list):
+        text = [normalize_text_block(line) for line in text]
+        text = [line for line in text if line.strip()]  # Remove empty or whitespace-only lines
+
     return text
 
-# usage
-# Read the scraped data from the file
+
+#%%
+# For file content read with '.read()'
 with open('scraped_data.txt', 'r', encoding='utf-8') as file:
-    scraped_data = file.readlines()
+    content = file.read()
+normalized_content = normalize_arabic(content)
 
-# Apply normalization to each piece of text
-normalized_data = [normalize_arabic(text) for text in scraped_data]
-
-# print 
-with open('scraped_data_result.txt', 'w', encoding='utf-8') as file:
-    for page_text in normalized_data:
-        print(page_text)
-        print("--------------------------------------------------")
-        file.write(page_text + '\n--------------------------------------------------\n')
+# For file content read with '.readlines()'
+with open('scraped_data.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+normalized_lines = normalize_arabic(lines)
 
