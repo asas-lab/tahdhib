35b3d91556c5d5f3586c94c88270f41164761edd
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2023-12-22 06:03:07 -0500

    Update normalize_arabic.py

--- normalize_arabic.py
+++ normalize_arabic.py
@@ -4,38 +4,33 @@ def normalize_arabic(text):
     # Arabic unicode block range
     arabic_range = '\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF'
 
+    # 1. Normalize Arabic diacritics (Tashkeel)
+    tashkeel_pattern = re.compile(r'[\u0617-\u061A\u064B-\u0652\u06D6-\u06DC\u06DF-\u06E8\u06EA-\u06ED]')
+    text = re.sub(tashkeel_pattern, '', text)
 
-    # Function to apply normalization rules to a single text block or line
-    def normalize_text_block(text):
-        # 1. Normalize Arabic diacritics (Tashkeel)
-        tashkeel_pattern = re.compile(r'[\u0617-\u061A\u064B-\u0652\u06D6-\u06DC\u06DF-\u06E8\u06EA-\u06ED]')
-        text = re.sub(tashkeel_pattern, '', text)
+    # 2. Standardize Arabic characters
+        
+    # Reason: there are variation of the same letetr 
+    # though they might held importance in dectation, their importance in meaning 
+    # -such as changing the meaning of the word when we return them to their original form-
+    # has less to no significance importance
+        
+    # Replace Alif variants with bare Alif (ٱ،إ،أ،آ)
+    text = re.sub(r'[\u0622\u0623\u0625\u0671\u0672\u0673\u0675]', '\u0627', text)
+    # Replace Yaa variants with bare Yaa (ي،ى)
+    text = re.sub(r'[\u0649]', '\u064A', text)
+    # Replace Taa Marbuta to Ha (ة،ه)
+    text = re.sub(r'ة\b', 'ه', text)
 
-        # 2. Standardize Arabic characters
-        # Replace Alif variants with bare Alif
-        text = re.sub(r'[\u0622\u0623\u0625\u0671\u0672\u0673\u0675]', '\u0627', text)
-        # Replace Yaa variants with bare Yaa
-        text = re.sub(r'[\u0649]', '\u064A', text)
+    # 3. Remove Tatweel (Kashida)
+    text = re.sub(r'\u0640', '', text)
 
-        # 3. Remove Tatweel (Kashida)
-        text = re.sub(r'\u0640', '', text)
-                
-        # 4.Normalize whitespace
-        text = ' '.join(text.split())
-
-        return text
-
-    # 5. Remove empty lines 
-    # if it is a text block
-    if isinstance(text, str):
-        text = normalize_text_block(text)
-        text = re.sub(r'\n+', '\n', text)  # Replace multiple newlines with a single one
-        text = re.sub(r'^\s*$', '', text, flags=re.MULTILINE)  # Remove empty lines
-
-    # if it is line in a list
-    elif isinstance(text, list):
-        text = [normalize_text_block(line) for line in text]
-        text = [line for line in text if line.strip()]  # Remove empty or whitespace-only lines
+    # 4. Remove empty lines 
+    text = re.sub(r'\n+', '\n', text)  # Replace multiple newlines with a single one
+    text = re.sub(r'^\s*$', '', text, flags=re.MULTILINE)  # Remove empty lines
+    
+    # 5.Normalize whitespace
+    text = ' '.join(text.split())
 
     return text
 
@@ -46,8 +41,4 @@ with open('scraped_data.txt', 'r', encoding='utf-8') as file:
     content = file.read()
 normalized_content = normalize_arabic(content)
 
-# For file content read with '.readlines()'
-with open('scraped_data.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-normalized_lines = normalize_arabic(lines)
 
