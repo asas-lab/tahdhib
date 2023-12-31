0af7174cdc169255d7e5ef4924490233f6e2ddd2
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2023-12-30 22:01:02 -0500

    Mini functions

--- normalize_arabic.py
+++ normalize_arabic.py
@@ -1,59 +1,52 @@
 import re
 
-def normalize_arabic(text):
-    # Arabic unicode block range
-    arabic_range = '\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF'
-
-    # 1. Normalize Arabic diacritics (Tashkeel)
+def remove_diacritics(text):
     tashkeel_pattern = re.compile(r'[\u0617-\u061A\u064B-\u0652\u06D6-\u06DC\u06DF-\u06E8\u06EA-\u06ED]')
-    text = re.sub(tashkeel_pattern, '', text)
-    
-    # 2.Remove non useful text
-    # HTML tags
+    return re.sub(tashkeel_pattern, '', text)
+
+def remove_non_useful_text(text):
     text = re.sub(r'<[^>]+>', '', text)
-    # URLs
-    # from a personal experiance in detecting posative/negative tweets
-    # not deleting URLs was helpful for the model!
-    # This only delete that start with 'http, https'
     text = re.sub(r'http[s]?://\S+', '', text)
-    
-    # 3. Remove repetition in letters like "هههههه، مرررحبا"
-    # more than 2 of the same character to two
-    text = re.sub(r'(.)\1+', r'\1\1', text)
-
-    # 4. Standardize Arabic characters
-    
-    # Replace Alif variants with bare Alif (ٱ،إ،أ،آ)
-    text = re.sub(r'[\u0622\u0623\u0625\u0671\u0672\u0673\u0675]', '\u0627', text)
-    # Replace Yaa variants with bare Yaa (ي،ى)
-    text = re.sub(r'[\u0649]', '\u064A', text)
-    # Replace Taa Marbuta to Ha (ة،ه)
-    text = re.sub(r'ة\b', 'ه', text)
-    # Replace (ؤ) with (و)
-    text = re.sub(r'ؤ', 'و', text)
-    # Replace (ئ) with (ي)
-    text = re.sub(r'ئ', 'ي', text)
-    # Delete (ء)
-    text = re.sub(r'ء', '', text)
-    # Delete (ال) from the beginning of the words
-    text = re.sub(r'\bال', '', text)
-
-    # 5. Remove Tatweel (Kashida)
-    text = re.sub(r'\u0640', '', text)
-    
-    # 6. Standrize numbers from western to Arabic -it can be the opposite-
-    western_to_arabic_numerals = str.maketrans('0123456789', '٠١٢٣٤٥٦٧٨٩')
-    text = text.translate(western_to_arabic_numerals)
-
-    # 7. Remove empty lines 
-    text = re.sub(r'\n+', '\n', text)  # Replace multiple newlines with a single one
-    text = re.sub(r'^\s*$', '', text, flags=re.MULTILINE)  # Remove empty lines
-    
-    # 8. Normalize whitespace
-    text = ' '.join(text.split())
-
     return text
 
+def remove_repeated_characters(text):
+    return re.sub(r'(.)\1+', r'\1\1', text)
+
+def standardize_arabic_characters(text):
+    text = re.sub(r'[\u0622\u0623\u0625\u0671\u0672\u0673\u0675]', '\u0627', text)
+    text = re.sub(r'[\u0649]', '\u064A', text)
+    text = re.sub(r'ة\b', 'ه', text)
+    text = re.sub(r'ؤ', 'و', text)
+    text = re.sub(r'ئ', 'ي', text)
+    text = re.sub(r'ء', '', text)
+    # there is an issue here coz if (ال) after (و) == and, it doesn't delete it coz it just delete (ال) if it exists at the beginning
+    text = re.sub(r'\bال', '', text)
+    return text
+
+def remove_tatweel(text):
+    return re.sub(r'\u0640', '', text)
+
+def standardize_numbers(text):
+    western_to_arabic_numerals = str.maketrans('0123456789', '٠١٢٣٤٥٦٧٨٩')
+    return text.translate(western_to_arabic_numerals)
+
+def remove_empty_lines(text):
+    text = re.sub(r'\n+', '\n', text)
+    return re.sub(r'^\s*$', '', text, flags=re.MULTILINE)
+
+def normalize_whitespace(text):
+    return ' '.join(text.split())
+
+def normalize_arabic(text):
+    text = remove_diacritics(text)
+    text = remove_non_useful_text(text)
+    text = remove_repeated_characters(text)
+    text = standardize_arabic_characters(text)
+    text = remove_tatweel(text)
+    text = standardize_numbers(text)
+    text = remove_empty_lines(text)
+    text = normalize_whitespace(text)
+    return text
 
 #%%
 # For file content read with '.read()'
